19e02331e7a55b36e9899cab030643eecae53d6c
prof-kayos <admin@profkayos.com>
2023-04-24 09:06:20 -0500

    Updated with installs and shortcut cleanup

--- my-decrap-install.ps1
+++ my-decrap-install.ps1
@@ -643,6 +643,43 @@ reg add "HKEY_USERS\.DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Pe
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Chat" /v "ChatIcon" /t "REG_DWORD" /d "3" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Communications" /v "ConfigureChatAutoInstall" /t "REG_DWORD" /d "0" /f
 
+#######################
+# Install Applications
+#######################
+
+winget install Bitwarden.Bitwarden -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Microsoft.WindowsTerminal -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install vscode -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Git.Git -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install beeper -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install OBSProject.OBSStudio -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install discord -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install 7zip.7zip -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install WireGuard.WireGuard -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install wireshark -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install winscp -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install cpu-z -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install hwinfo -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Valve.Steam -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Nvidia.Broadcast -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Nvidia.GeForceExperience -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install "Xbox Accessories" -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Adobe.Acrobat.Reader.64-bit -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install "HP Smart" -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Microsoft.DotNet.DesktopRuntime.6 -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install handbrake -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install paint.net -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install openshot -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install SlackTechnologies.Slack -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Citrix -h --accept-source-agreements --accept-package-agreements --disable-interactivity
+
+###########################
+# Remove Desktop Shortcuts
+###########################
+
+Remove-Item C:\Users\*\Desktop\*lnk –Force
+Remove-Item C:\Users\*\OneDrive\Desktop\*lnk –Force
+
 ##########
 # Restart
 ##########
